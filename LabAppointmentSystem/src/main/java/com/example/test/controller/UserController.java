cb8225e989cdc1d27b1f6a1002beb4efd6d5b119
RainiWijewantha <112390022+RainiWijewantha@users.noreply.github.com>
2024-03-02 13:22:12 +0530

    Backend Changed

--- LabAppointmentSystem/src/main/java/com/example/test/controller/UserController.java
+++ LabAppointmentSystem/src/main/java/com/example/test/controller/UserController.java
@@ -3,7 +3,6 @@ package com.example.test.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.test.model.UserModel;
@@ -11,59 +10,78 @@ import com.example.test.service.UserService;
 
 @Controller
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@GetMapping("/")
 	public String dashboard() {
 		return "PatientDashboard";
 	}
-	
+
 	@GetMapping("/requestMedicalTest")
 	public String requestMedicalTest() {
 		return "RequestMedicalTest";
 	}
-	
+
 	@GetMapping("/viewAppointmentDetails")
 	public String viewAppointmentDetails() {
 		return "ViewAppointmentDetails";
 	}
-	
+
 	@GetMapping("/reports")
 	public String reports() {
 		return "Reports";
 	}
-	
+
 	@GetMapping("/register")
 	public String patientRegister() {
 		return "Register";
 	}
-	
+
 	@GetMapping("/message")
 	public String message() {
 		return "Message";
 	}
-	
+
 	@PostMapping("/next")
 	public String displayMessage() {
 		return "redirect:/message";
 	}
-	
+
 	@GetMapping("/patientLogin")
 	public String patientLogin() {
 		return "PatientLogin";
 	}
-	
+
 	@GetMapping("/payment")
 	public String payment() {
 		return "Payment";
 	}
-	
-	@GetMapping("/afterLogin")
+
+	@PostMapping("/afterLogin")
 	public String afterLogin() {
 		return "redirect:/payment";
 	}
 
+	@GetMapping("/forgotPassword")
+	public String forgotPassword() {
+		return "ForgotPassword";
+	}
+
+	@PostMapping("/forgotPassword")
+	public String processForgotPassword() {
+		return "redirect:/newPassword";
+	}
+
+	@GetMapping("/newPassword")
+	public String newPassword() {
+		return "NewPassword";
+	}
+
+	@PostMapping("/newPassword") 
+	public String processNewPassword() {
+		return "redirect:/patientLogin"; 
+	}
 
 }
