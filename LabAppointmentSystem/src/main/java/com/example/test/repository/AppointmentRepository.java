94aa22672c5430b30b3d6d8b505d7290a312b011
RainiWijewantha <112390022+RainiWijewantha@users.noreply.github.com>
2024-03-18 02:51:07 +0530

    Changes Added

--- LabAppointmentSystem/src/main/java/com/example/test/repository/AppointmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.test.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.test.model.AppointmentsModel;
-
-public interface AppointmentRepository extends JpaRepository<AppointmentsModel, String>{
-
-}
